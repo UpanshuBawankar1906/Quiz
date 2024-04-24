ef05f4e47b578a1c6fdb4a34ad2bc669f0637d87
upanshu bawankar <619bawankarupanshu@gmail.com>
2024-04-24 11:27:44 +0530

    commit

--- QuizIndia/QuizIndia/src/test/java/com/example/QuizIndia/QuizIndiaApplicationTests.java
+++ QuizIndia/QuizIndia/src/test/java/com/example/QuizIndia/QuizIndiaApplicationTests.java
@@ -1,13 +1,22 @@
 package com.example.QuizIndia;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 class QuizIndiaApplicationTests {
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
-	void contextLoads() {
+	void shouldReturnDefaultMessage() throws Exception {
+		mockMvc.perform(get("/")).andExpect(status().isOk()).andExpect(content().string("Hello World!"));
 	}
 
 }
