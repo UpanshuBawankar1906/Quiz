5a0f45e60f239830818eef9572df55ac977ca071
upanshu bawankar <619bawankarupanshu@gmail.com>
2024-04-24 11:26:59 +0530

    commit

--- QuizIndia/QuizIndia/src/test/java/com/example/QuizIndia/repo/QuestionsRepoTest.java
+++ QuizIndia/QuizIndia/src/test/java/com/example/QuizIndia/repo/QuestionsRepoTest.java
@@ -1,24 +1,19 @@
 package com.example.QuizIndia.repo;
-
 import com.example.QuizIndia.model.Questions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class QuestionsRepoTest {
-
     @Mock
     private QuestionsRepo questionsRepo;
-
     @InjectMocks
     private QuestionsRepoTest repoTest;
-
     @Test
     public void insert() {
         Questions expectedQuestionsObject = new Questions(3L,"what is JAVA ?","language",
@@ -27,6 +22,6 @@ public class QuestionsRepoTest {
         when(questionsRepo.insert(expectedQuestionsObject)).thenReturn(expectedQuestionsObject);
         Questions actualQuestionsObject = questionsRepo.insert(expectedQuestionsObject);
         assertThat(actualQuestionsObject).usingRecursiveComparison()
-                .ignoringFields("postId").isEqualTo(expectedQuestionsObject);
+                .ignoringFields("Id").isEqualTo(expectedQuestionsObject);
     }
 }
